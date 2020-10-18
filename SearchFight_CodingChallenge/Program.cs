b721bde2681679f96751799ba1680289393c6788
unknown <digiespiri93@hotmail.com>
2020-10-17 23:44:22 -0500

    First commit

---
+++ SearchFight_CodingChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SearchFight_CodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine(firstName);
+
+            int cantJava, cantCSharp = 0;
+
+
+            Console.WriteLine("Google Winner:");
+            Console.WriteLine("MSN Search Winner:");
+            Console.WriteLine("Total Winner:");
+        }
+    }
+}
